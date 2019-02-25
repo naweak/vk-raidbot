c49c15b6e9fdbd44a262c9911b664e70a7834c42
Алексей <33986066+naweak@users.noreply.github.com>
2019-02-25 22:24:59 +0300

    run js from message

--- bot/server.js
+++ bot/server.js
@@ -4,6 +4,7 @@ var axios = require('axios')
 var shellExec = require('shell-exec')
 var { stringify } = require('querystring')
 var express = require('express')
+var fs = require('fs')
 
 // API params
 var vkEndpoint = 'https://api.vk.com/method'
@@ -38,11 +39,37 @@ function updateHandle (update) {
         console.log(admins)
         var issueRexp = new RegExp('^\.issue (.+)', 'i')
         var issueMatches = issueRexp.exec(message)
+        var nodeRexp = new RegExp('^\.(node(js){0,1}|js) (.+)', 'i')
+        var nodeMatches = nodeRexp.exec(message)
         if (issueMatches && isAdmin(fromId)) {
             var command = issueMatches[1]
             command = command.replace('»', '>>')
             command = command.replace('—', '--')
             shellExec(command).then(out => {
+                if (out.stdout)
+                    result = out.stdout
+                else
+                    result = out.stderr
+                axios.post(`${vkEndpoint}/messages.send`, stringify({
+                    access_token,
+                    v,
+                    message: result,
+                    random_id: getRandomId(),
+                    peer_id: peerId
+                })).then(console.log).catch(console.log)
+            })
+        }
+        else if (nodeMatches && isAdmin(fromId)) {
+            console.log(nodeMatches)
+            var code = nodeMatches[3]
+            code = code.replace('»', '>>')
+            code = code.replace('—', '--')
+            code = code.replace('<br>', '\n')
+            fs.writeFile('exec.js', code, e => {
+                if (e)
+                    console.log(e)
+            })
+            shellExec('node exec.js').then(out => {
                 if (out.stdout)
                     result = out.stdout
                 else
